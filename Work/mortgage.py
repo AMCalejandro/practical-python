5bc8fad2aa4cfd30055e9371c364ca4e5314c491
AMCalejandro <alejandro.carrasco.20@ucl.ac.uk>
2021-10-08 13:00:28 +0100

    Adding the mortgage.py excercise - 1.3 Section-

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,40 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+# mortgage.py
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+month = 0
+
+extra_payment_start_month = int(input('Enter the start month for the extra payment:'))
+extra_payment_end_month = int(input('Enter the end month for the extra payment:'))
+extra_payment =  int(input('Enter the quantity of the extra payment:'))
+
+
+while (principal > 0):
+    month = month + 1
+
+    if (month >= extra_payment_start_month) & (month <= extra_payment_end_month):
+        if (principal * (1+rate/12)) <= (payment + extra_payment):
+            last_payment = principal * (1+rate/12)
+            principal = last_payment - last_payment
+            total_paid = total_paid + principal
+        else:
+            principal = principal * (1+rate/12) - (payment + extra_payment)
+            total_paid = total_paid + payment + extra_payment
+    else:
+        if (principal * (1+rate/12)) <= payment:
+            last_payment = principal * (1+rate/12)
+            principal = last_payment - last_payment
+        else:
+            principal = principal * (1+rate/12) - payment
+            total_paid = total_paid + payment
+
+    print(month, total_paid, round(principal, ndigits = 2))
+
+
+print('Total paid', total_paid, "\n", 'months required', month)
