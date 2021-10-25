e70b4cab523eaa4611e911557e54deb605f9ff9b
AMCalejandro <alejandro.carrasco.20@ucl.ac.uk>
2021-10-25 16:24:06 +0100

    Adding the pcost.py exercise

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,14 @@
 # pcost.py
 #
 # Exercise 1.27
+
+sum = 0 # Initializing a variable to 0
+#with open('Data/portfolio.csv', 'rt') as file:
+file = open('Data/portfolio.csv', 'rt')
+header = next(file) # Removing the header of the file
+for line in file:
+    #print(line)
+    list = line.split(',')
+    sum += (int(list[1])*float(list[2])) # Transforming data and calculating the price of all stockes
+
+print(f'Total cost {sum}')
