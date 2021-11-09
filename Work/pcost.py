bf427d80fa390c35661d7afcdf9ea0ae6db39a7e
AMCalejandro <alejandro.carrasco.20@ucl.ac.uk>
2021-11-09 13:16:00 +0000

    Reading csv files with appropriate functions from csv module

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,13 +2,17 @@
 #
 # Exercise 1.27
 
+import csv
 sum = 0 # Initializing a variable to 0
 #with open('Data/portfolio.csv', 'rt') as file:
-file = open('Data/portfolio.csv', 'rt')
-header = next(file) # Removing the header of the file
-for line in file:
+file = open('Data/portfolio.csv')
+rows = csv.reader(file) 
+header = next(rows) # Removing the header of the file
+for line in rows:
     #print(line)
-    list = line.split(',')
-    sum += (int(list[1])*float(list[2])) # Transforming data and calculating the price of all stockes
+    #list = line.split(',')
+    sum += (int(line[1])*float(line[2])) # Transforming data and calculating the price of all stockes
 
 print(f'Total cost {sum}')
+
+file.close()
--- Work/pcost_function.py
+++ Work/pcost_function.py
@@ -1,18 +1,21 @@
 # Turning the pcost.py script into a function
 
+
 def portfolio_cost(filename):
-     sum = 0 # Initializing a variable to 0
-     file = open(filename, 'rt')
-     header = next(file) # Removing the header of the file
-     line_count=0 
-     try:
-         for line in file:
-             list = line.split(',')
-             sum += (int(list[1])*float(list[2]))
-             line_count += 1
-     except:
-         print("Missing integers in line:",int(line_count)+1,":", line)
-     return(sum)
+    import csv
+    sum = 0 # Initializing a variable to 0
+    file = open(filename)
+    rows = csv.reader(file)
+    headers = next(rows)
+    line_count=0
+    try:
+        for line in rows:
+            #list = line.split(',')
+            sum += (int(line[1])*float(line[2]))
+            line_count += 1
+    except:
+        print("Missing integers in line:",int(line_count)+1,":", line)
+    return(sum)
 
 
 #print("Total cost",  portfolio_cost('Data/portfolio.csv'))
