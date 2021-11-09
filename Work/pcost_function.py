ca5cf94e466b0b007083ec6fcb6d0bc68c20fc08
AMCalejandro <alejandro.carrasco.20@ucl.ac.uk>
2021-11-09 13:23:21 +0000

    Try/Except bug solved

--- Work/pcost_function.py
+++ Work/pcost_function.py
@@ -8,14 +8,15 @@ def portfolio_cost(filename):
     rows = csv.reader(file)
     headers = next(rows)
     line_count=0
-    try:
-        for line in rows:
+    for line in rows:
+        try:
             #list = line.split(',')
             sum += (int(line[1])*float(line[2]))
             line_count += 1
-    except:
-        print("Missing integers in line:",int(line_count)+1,":", line)
+        except:
+            print("Missing integers in line:",int(line_count)+1,":", line)
     return(sum)
+    file.close()
 
 
 #print("Total cost",  portfolio_cost('Data/portfolio.csv'))
