bce1926ec35d489e3c6a7940c0d1d93f3bfdf7e1
AMCalejandro <alejandro.carrasco.20@ucl.ac.uk>
2022-01-18 14:03:56 +0000

    Adding the function to parse CSV into a list of dictionaries

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,23 @@
 # fileparse.py
 #
 # Exercise 3.3
+
+import csv
+
+def parse_csv(filename):
+    '''
+    Parse a CSV file into a list of dictionaries
+    '''
+    with open(filename) as f:
+        rows = csv.reader(f)
+        header = next(rows)
+        #print(header)
+        records = []
+        for row in rows:
+            if not row:
+                continue
+            records.append(dict(zip(header, row)))
+    return records
+
+#print(parse_csv("Data/portfolio.csv"))
+
