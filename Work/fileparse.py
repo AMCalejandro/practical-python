7e09525d419a866d0201d3cc87e2bdd6be87f73c
AMCalejandro <alejandro.carrasco.20@ucl.ac.uk>
2022-01-19 13:11:23 +0000

    Allowing the fileparse function to take a specific list of columns

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -4,20 +4,51 @@
 
 import csv
 
-def parse_csv(filename):
+#def parse_csv(filename, select = None):
+#    '''
+#    Parse a CSV file into a list of dictionaries
+#    '''
+#    with open(filename) as f:
+#        rows = csv.reader(f)
+#        header = next(rows)
+#        if select:
+#           indices = [header.index(col_interest) for col_interest in select]
+#           records = [{column: row[index] for column,index in zip(select, indices)}  for row in rows]
+#
+#        else:
+#            records = []
+#            for row in rows:
+#                if not row:
+#                    continue
+#                records.append(dict(zip(header, row)))
+#    return records
+#print(parse_csv("Data/portfolio.csv"))
+
+
+def parse_csv(filename, select = None):
     '''
     Parse a CSV file into a list of dictionaries
     '''
     with open(filename) as f:
         rows = csv.reader(f)
-        header = next(rows)
-        #print(header)
+        headers = next(rows)
+
+        if select:
+            indices = [headers.index(col_interest) for col_interest in select]
+            headers = select
+        else:
+            indices = []
+
         records = []
         for row in rows:
             if not row:
                 continue
-            records.append(dict(zip(header, row)))
+            if indices:
+                row = [row[index] for index in indices]
+
+            record = dict(zip(headers, row))
+            records.append(record)
+
     return records
 
-#print(parse_csv("Data/portfolio.csv"))
 
