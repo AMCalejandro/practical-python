2972bdf5eae1098d2eceda962b0aed1f14f650d9
AMCalejandro <alejandro.carrasco.20@ucl.ac.uk>
2022-01-24 14:22:34 +0000

    Raising, catching, and silencing errors

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -25,12 +25,16 @@ import csv
 #print(parse_csv("Data/portfolio.csv"))
 
 
-def parse_csv(filename, select = None, types = None, has_headers = True, delimiter = " "):
+def parse_csv(filename, select = None, types = None, has_headers = True, delimiter = " ", silence_errors = False):
     '''
     Parse a CSV file into a list of dictionaries
     '''
     with open(filename) as f:
         rows = csv.reader(f, delimiter = delimiter)
+
+        if select and not has_headers:
+             raise RuntimeError('select requires column headers')
+
         if has_headers:
             headers = next(rows)
 
@@ -41,17 +45,28 @@ def parse_csv(filename, select = None, types = None, has_headers = True, delimit
             indices = []
 
         records = []
-        for row in rows:
+#        for row in rows:
+        for row_index,row in enumerate(rows):
             if not row:
                 continue
             if indices:
                 row = [row[index] for index in indices]
+
+
             if types:
-                row = [func(val) for func,val in zip(types, row)]
+#                row = [func(val) for func,val in zip(types, row)]
+                 try:
+                     row = [func(val) for func,val in zip(types, row)]
+                 except ValueError as e:
+                     if not silence_errors:
+                         print(f'Row {row_index + 1}: Couldn\'t convert {row}')
+                         print(f'Row {row_index + 1}: Reason invalid literal for int() with base 10: \'\'')
+                     continue
+
             if has_headers:
                 record = dict(zip(headers, row))
                 records.append(record)
-            if not has_headers:
+            else:
                 records.append(tuple(row))
 
     return records
