442be63a8b55a4a5b7b6dfe6ab015ab54a5df54a
AMCalejandro <alejandro.carrasco.20@ucl.ac.uk>
2022-01-31 13:21:28 +0000

    Using parse_csv from fileparse module within pcost.py script

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,6 +1,5 @@
 # Turning the pcost.py script into a function
 
-
 #def portfolio_cost(filename):
 #    import csv
 #    import sys
@@ -21,23 +20,36 @@
 
 # Improving the function. Using enumerate to track the index and
 # Using zip to create a dictionary with the header and one row at a time
+#def portfolio_cost(filename):
+#    import csv
+#    import sys
+#    sum = 0 # Initializing a variable to 0
+#    file = open(filename)
+#    rows = csv.reader(file)
+#    headers = next(rows)
+#    for index, line in enumerate(rows, start=1):
+#        record = dict(zip(headers,line))
+        #print(record)
+#        try:
+#            sum += float(record['price'])*int(record['shares'])
+#        except:
+#            print("Missing integers in line:", index)
+#    return(sum)
+#    file.close()
+
+import fileparse
 def portfolio_cost(filename):
-    import csv
-    import sys
-    sum = 0 # Initializing a variable to 0
-    file = open(filename)
-    rows = csv.reader(file)
-    headers = next(rows)
-    for index, line in enumerate(rows, start=1):
-        record = dict(zip(headers,line))
+    sum = 0
+    portfolio_dictlist = fileparse.parse_csv(filename, delimiter = ",")
+    for index in range(len(portfolio_dictlist)):
+        record = portfolio_dictlist[index]
         #print(record)
         try:
             sum += float(record['price'])*int(record['shares'])
         except:
             print("Missing integers in line:", index)
-    return(sum)
-    file.close()
 
+    return(sum)
 
 
 
