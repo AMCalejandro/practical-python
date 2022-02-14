4a05fe30ccbb3d356ffe4101397e44c32d603628
AMCalejandro <alejandro.carrasco.20@ucl.ac.uk>
2022-02-14 14:06:10 +0000

    Adding some methods to the Class stocK

--- Work/stock.py
+++ Work/stock.py
@@ -1,5 +1,20 @@
 class Stock:
+    '''
+    The instance of a stock holding
+    '''
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
         self.price = price
+
+    def cost(self):
+        '''
+        Method that returns the cost of current shares according to market price
+        '''
+        return self.shares * self.price
+
+    def sell(self, sells):
+        '''
+        A method to update the total shares after selling some
+        '''
+        self.shares -= sells
