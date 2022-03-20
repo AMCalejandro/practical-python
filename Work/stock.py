de6d3040b61d32e7e975acc9be0e248af8b7eefc
AMCalejandro <alejandro.carrasco.20@ucl.ac.uk>
2022-03-20 13:31:32 +0000

    Adding __repr__ magic method

--- Work/stock.py
+++ Work/stock.py
@@ -7,6 +7,10 @@ class Stock:
         self.shares = shares
         self.price = price
 
+    def __repr__(self):
+        return f'Stock({self.name!r}, {self.shares!r}, {self.price!r})'
+
+
     def cost(self):
         '''
         Method that returns the cost of current shares according to market price
