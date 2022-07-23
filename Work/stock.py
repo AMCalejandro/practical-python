9f69c259633a30fde7440ac2f535719596952fc4
AMCalejandro <alejandro.carrasco.20@ucl.ac.uk>
2022-07-23 09:53:51 +0100

    Rewriting Stock class to avoid code repetition

--- Work/stock.py
+++ Work/stock.py
@@ -1,10 +1,18 @@
+from typedproperty import typedproperty, String, Integer, Float
+
 class Stock:
     '''
     The instance of a stock holding
     '''
 
     # Restricting the attributes names with __slots__
-    __slots__ = ('name','_shares','price')
+    __slots__ = ('_name', '_shares','_price')
+
+    name = String('name')
+    shares = Integer('shares')
+    price = Float('price')
+
+
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
@@ -13,30 +21,28 @@ class Stock:
     def __repr__(self):
         return f'Stock({self.name!r}, {self.shares!r}, {self.price!r})'
 
-    @property
-    def cost(self):
-        '''
-        Method that returns the cost of current shares according to market price
-        '''
-        return self.shares * self.price
+#    @property
+#    def cost(self):
+#        '''
+#        Method that returns the cost of current shares according to market price
+#        '''
+#        return self.shares * self.price
 
-    def sell(self, sells):
-        '''
-        A method to update the total shares after selling some
-        '''
-        self.shares -= sells
+#    def sell(self, sells):
+#        '''
+#        A method to update the total shares after selling some
+#        '''
+#        self.shares -= sells
 
-    @property
-    def shares(self):
-        return self._shares
+#    @property
+#    def shares(self):
+#        return self._shares
 
-    @shares.setter
-    def shares(self, value):
-        if not isinstance(value, int):
-            raise TypeError("Expected int")
-        self._shares = value
-
-
+#    @shares.setter
+#    def shares(self, value):
+#        if not isinstance(value, int):
+#            raise TypeError("Expected int")
+#        self._shares = value
 
 
 
@@ -44,4 +50,3 @@ class Stock:
 #class NewStock(Stock):
 #    def yow(self):
 #        print("Yow!")
-
